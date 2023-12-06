2cc6f27a264d63a6b2af764523a12df05bc9f546
Vladimir Parfinenko <vladimir.parfinenko@jetbrains.com>
2023-12-06 13:16:16 +0100

    Utils: measureTime, better class name

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.time.measureTimedValue
 
 /**
  * Reads lines from the given input txt file.
@@ -22,22 +23,23 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 fun Any?.println() = println(this)
 
 fun test(vararg parts: (List<String>) -> Long) {
-    val className = Throwable().stackTrace[1].className
-    check(className.startsWith("Day"))
-    check(className.endsWith("Kt"))
-
+    val className = Throwable().stackTrace
+        .map { it.className }
+        .dropWhile { !it.startsWith("Day") || !it.endsWith("Kt") }
+        .first()
     val day = className.substringBefore("Kt")
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("${day}_test")
-    val realInput = readInput(day)
-
     for ((i, p) in parts.withIndex()) {
-        for ((kind, input) in listOf(Pair("test", testInput), Pair("real", realInput))) {
-            print("part${i + 1}, $kind: ")
-            runCatching { p(input) }
-                .onSuccess { println(it) }
-                .onFailure { it.printStackTrace(System.out) }
+        for ((kind, inputSuffix) in listOf(Pair("test", "_test"), Pair("real", ""))) {
+            runCatching { readInput("$day$inputSuffix") }
+                .onSuccess { input ->
+                    print("part${i + 1}, $kind: ")
+                    val (result, time) = measureTimedValue { runCatching { p(input) } }
+                    print("${result.getOrElse { "ERROR" }} (took ${time.inWholeMilliseconds} ms)")
+                    result
+                        .onSuccess { println() }
+                        .onFailure { it.printStackTrace(System.out) }
+                }
         }
     }
 }

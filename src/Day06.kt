49a9a1187bb8af9112ea76a28a4d91da09964de7
Vladimir Parfinenko <vladimir.parfinenko@jetbrains.com>
2023-12-06 10:08:36 +0100

    Utils: get rid of test running copy-pasting

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,33 +1,24 @@
-fun main() {
-    fun process(races: List<Pair<Long, Long>>): Long {
-        return races.fold(1) { acc, (time, distance) ->
-            acc * (0 until time).count { hold ->
-                val speed = hold
-                speed * (time - hold) > distance
-            }
+fun main() = test(::part1, ::part2)
+
+private fun process(races: List<Pair<Long, Long>>): Long {
+    return races.fold(1) { acc, (time, distance) ->
+        acc * (0 until time).count { hold ->
+            val speed = hold
+            speed * (time - hold) > distance
         }
     }
+}
 
-    fun part1(input: List<String>): Long {
-        val (times, distances) = input.map {
-            it.split(Regex(" +")).drop(1).map { it.toLong() }
-        }
-        return process(times zip distances)
+private fun part1(input: List<String>): Long {
+    val (times, distances) = input.map {
+        it.split(Regex(" +")).drop(1).map { it.toLong() }
     }
+    return process(times zip distances)
+}
 
-    fun part2(input: List<String>): Long {
-        val (time, distance) = input.map {
-            it.substringAfter(":").replace(" ", "").toLong()
-        }
-        return process(listOf(Pair(time, distance)))
+private fun part2(input: List<String>): Long {
+    val (time, distance) = input.map {
+        it.substringAfter(":").replace(" ", "").toLong()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
-    println(part1(testInput))
-    println(part2(testInput))
-
-    val input = readInput("Day06")
-    part1(input).println()
-    part2(input).println()
+    return process(listOf(Pair(time, distance)))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -18,4 +18,23 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 /**
  * The cleaner shorthand for printing output.
  */
+@Deprecated("just don't use it")
 fun Any?.println() = println(this)
+
+fun test(part1: (List<String>) -> Long, part2: (List<String>) -> Long) {
+    val className = Throwable().stackTrace[1].className
+    check(className.startsWith("Day"))
+    check(className.endsWith("Kt"))
+
+    val day = className.substringBefore("Kt")
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("${day}_test")
+    val realInput = readInput(day)
+
+    for ((i, p) in listOf(part1, part2).withIndex()) {
+        for ((kind, input) in listOf(Pair("test", testInput), Pair("real", realInput))) {
+            println("part${i + 1}, $kind: ${p(input)}")
+        }
+    }
+}

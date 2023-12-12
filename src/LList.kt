e00bdb34d982bd8a2ad9147de49b3802b346e20e
Vladimir Parfinenko <vladimir.parfinenko@jetbrains.com>
2023-12-12 13:35:20 +0100

    Day12: linked list, extension functions with nullable this, tailrec, memoization

---
+++ src/LList.kt
@@ -0,0 +1,29 @@
+class LList<T>(val head: T, val tail: LList<T>?) {
+    // For debugging purposes.
+    override fun toString(): String =
+        (this as LList<T>?).toString()
+}
+
+fun <T> LList<T>?.toList(): List<T> =
+    when (this) {
+        null ->
+            emptyList<T>()
+        else ->
+            buildList<T> {
+                var cur: LList<T>? = this@toList
+                while (cur != null) {
+                    add(cur.head)
+                    cur = cur.tail
+                }
+            }
+    }
+
+fun <T> LList<T>?.toString(): String =
+    this.toList().toString()
+
+fun <T> Iterable<T>.toLList(): LList<T>? =
+    this.iterator().toLList()
+
+fun <T> Iterator<T>.toLList(): LList<T>? =
+    if (this.hasNext()) LList(this.next(), this.toLList()) else null
+

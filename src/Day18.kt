2b1b5957b8b632bd725efe78c4aec961e4113639
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2023-12-18 22:44:41 +0100

    Day18: good algorithm wins fast coding

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,6 +1,8 @@
+import Dir.*
+
 fun main() = test(
-    { solve(2_000, it.map(::parseLinePart1)) },
-    { solve(20_000, it.map(::parseLinePart2)) },
+    { solve(it.map(::parseLinePart1)) },
+    { solve(it.map(::parseLinePart2)) },
 )
 
 private val INPUT_REGEX_PART1 = """(.) (\d+) .*""".toRegex()
@@ -19,78 +21,13 @@ private fun parseLinePart2(line: String): Pair<Int, Dir> {
     return Pair(lenHex.toInt(16), dir)
 }
 
-
-private fun solve(mapSize: Int, commands: List<Pair<Int, Dir>>): Long {
-    // Hope, it would be enough.
-    val map = Array(mapSize) { Array(mapSize) { false } }
-
-    // Count all filled cells.
-    var fill = 0L
-    fun visit(pos: Pair<Int, Int>): Boolean {
-        if (map[pos]) return false // Already visited.
-        map[pos] = true
-        fill++
-        return true
-    }
-
-    // Detect an orientation of the cycle.
-    var firstDir: Dir? = null
-    var prevDir: Dir? = null
-    var lefts = 0
-    var rights = 0
-
-    fun changeDir(newDir: Dir) {
-        if (prevDir != null) {
-            when (newDir) {
-                prevDir!!.left -> lefts++
-                prevDir!!.right -> rights++
-                else -> {}
-            }
-        } else {
-            firstDir = newDir
+private fun solve(commands: List<Pair<Int, Dir>>): Long {
+    return commands.fold(Pair(0L, 1L)) { (offset, area), (len, dir) ->
+        when (dir) {
+            RIGHT -> Pair(offset + len, area + len)
+            LEFT  -> Pair(offset - len, area)
+            DOWN  -> Pair(offset,       area + len * (offset + 1))
+            UP    -> Pair(offset,       area - len * offset)
         }
-        prevDir = newDir
-    }
-
-    // "Draw" cycle.
-    val start = Pair(mapSize/2, mapSize/2)
-    visit(start)
-    var cur = start
-
-    for ((len, dir) in commands) {
-        repeat(len) {
-            cur = moveInDir(cur, dir)
-            visit(cur)
-        }
-
-        changeDir(dir)
-    }
-    changeDir(firstDir!!)
-    assert(cur == start)
-
-    // And now "fill" it.
-    fun fill(pos: Pair<Int, Int>) {
-        if (!visit(pos)) return
-
-        fill(moveInDir(pos, Dir.UP))
-        fill(moveInDir(pos, Dir.DOWN))
-        fill(moveInDir(pos, Dir.LEFT))
-        fill(moveInDir(pos, Dir.RIGHT))
-    }
-
-    val insideDir: (Dir) -> Dir = when (rights - lefts) {
-        4 -> {{ it.right }}
-        -4 -> {{ it.left }}
-        else -> throw IllegalStateException()
-    }
-
-    for ((len, dir) in commands) {
-        val inside = insideDir(dir)
-        repeat(len) {
-            cur = moveInDir(cur, dir)
-            fill(moveInDir(cur, inside))
-        }
-    }
-
-    return fill
+    }.second
 }

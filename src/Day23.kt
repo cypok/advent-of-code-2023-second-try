2924fba9497120e08a369aab03431b6f212a4690
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2023-12-24 09:34:39 +0100

    Day23: optimize part2, takes 10 min
    * Don't add intermediate single track points to visited
    * Less code in single track case

--- src/Day23.kt
+++ src/Day23.kt
@@ -24,12 +24,10 @@ private fun solve(input: List<String>, respectSlopes: Boolean): Long {
     tailrec fun traverse(pos: Point, dir: Dir, len: Long, visited: MutableSet<Point>, alternativeLen: Long): Long {
         if (pos == finish) return max(len, alternativeLen)
 
-        visited += pos
-
         fun canGoTo(nextPos: Point, dir: Dir): Boolean {
-            val ch = map.getOrNull(nextPos)
+            val ch = map[nextPos]
 
-            if (ch == null || ch == '#') return false
+            if (ch == '#') return false
             if (nextPos in visited) return false
 
             if (respectSlopes) {
@@ -53,18 +51,31 @@ private fun solve(input: List<String>, respectSlopes: Boolean): Long {
         if (nextDirs.isEmpty()) return alternativeLen
 
         val firstDir = nextDirs.first()
-        val forkedDirs = nextDirs.drop(1)
+        val forkedLen =
+            if (nextDirs.size == 1) {
+                // Single track case.
+                alternativeLen
+            } else {
+                val forkedDirs = nextDirs.drop(1)
 
-        // We must prepare a new set for every fork, except the first one.
-        val forkedSets = forkedDirs.indices.map { visited.toMutableSet() }
-        val forkedLen = (forkedDirs zip forkedSets).maxOfOrNull { (nextDir, nextVisited) ->
-            @Suppress("NON_TAIL_RECURSIVE_CALL")
-            traverse(pos.moveInDir(nextDir), nextDir,
-                len + 1,
-                nextVisited,
-                alternativeLen)
-        } ?: alternativeLen
-        assert(forkedLen >= alternativeLen)
+                // We can skip this step if we are on a single track.
+                visited += pos
+
+                val forkedLen = forkedDirs
+                    // We must prepare a new set for every fork, except the first one.
+                    .map { Pair(it, visited.toMutableSet()) }
+                    .maxOf { (nextDir, nextVisited) ->
+                        @Suppress("NON_TAIL_RECURSIVE_CALL")
+                        traverse(
+                            pos.moveInDir(nextDir), nextDir,
+                            len + 1,
+                            nextVisited,
+                            alternativeLen
+                        )
+                    }
+                assert(forkedLen >= alternativeLen)
+                forkedLen
+            }
 
         return traverse(pos.moveInDir(firstDir), firstDir,
             len + 1,

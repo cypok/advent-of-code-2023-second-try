3db23f344d527a5c484ae2353e42cc8ca228c7fc
Vladimir Parfinenko <vladimir.parfinenko@jetbrains.com>
2024-12-03 09:48:13 +0100

    Year2018-Day1: use Collection.cycle()

---
+++ src/year2018/Day1.kt
@@ -0,0 +1,26 @@
+package year2018
+
+import utils.*
+
+// Task description:
+//   https://adventofcode.com/2018/day/1
+
+fun main() = test(
+    ::solve1,
+    ::solve2,
+)
+
+private fun solve1(input: List<String>): Long =
+    input.sumOf { it.toLong() }
+
+private fun solve2(input: List<String>): Long {
+    var cur = 0L
+    val visited = mutableSetOf<Long>()
+    for (line in input.cycle()) {
+        cur += line.toLong()
+        if (!visited.add(cur)) {
+            return cur
+        }
+    }
+    error("should not reach here")
+}

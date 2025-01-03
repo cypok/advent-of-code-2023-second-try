f84ad1ec3358b9e1ca4dcc8f943c460aa148a7f9
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2025-01-03 12:07:01 +0100

    All-runner: show total time and don't report future days

--- src/All.kt
+++ src/All.kt
@@ -2,7 +2,10 @@ import utils.IS_BATCH_RUN
 import utils.cartesianProduct
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
+import java.time.LocalDate
+import java.time.Month
 import java.time.Year
+import kotlin.time.measureTime
 
 fun main() {
     IS_BATCH_RUN = true
@@ -10,32 +13,46 @@ fun main() {
     val lookup = MethodHandles.lookup()
     val mainName = "main"
     val mainMT = MethodType.methodType(Class.forPrimitiveName("void"))
-    var lastWasShort = false
-    for (year in 2015..Year.now().value) {
-        for (day in 1..25) {
-            val nameParts = listOf(
-                listOf("year$year."),
-                listOf("Day"),
-                listOf(day.toString(), "%02d".format(day)),
-                listOf("Kt", "")
-            )
-            val cls = nameParts.cartesianProduct().firstNotNullOfOrNull { parts ->
-                runCatching { lookup.findClass(parts.joinToString("")) }.getOrNull()
-            }
 
-            if (cls != null) {
-                if (lastWasShort) {
-                    println()
-                    lastWasShort = false
+    val today = LocalDate.now()
+
+    var totalDays = 0
+    var lastWasShort = false
+    val totalTime = measureTime {
+        years@for (year in 2015..Year.now().value) {
+            for (day in 1..25) {
+                val dayDate = LocalDate.of(year, Month.DECEMBER, day)
+                if (dayDate > today) break@years
+
+                val nameParts = listOf(
+                    listOf("year$year."),
+                    listOf("Day"),
+                    listOf(day.toString(), "%02d".format(day)),
+                    listOf("Kt", "")
+                )
+                val cls = nameParts.cartesianProduct().firstNotNullOfOrNull { parts ->
+                    runCatching { lookup.findClass(parts.joinToString("")) }.getOrNull()
                 }
-                println("Year $year, Day $day")
-                val method = lookup.findStatic(cls, mainName, mainMT)
-                method.invokeExact()
-                println()
-            } else {
-                println("Year $year, Day $day: not yet")
-                lastWasShort = true
+
+                if (cls != null) {
+                    totalDays++
+                    if (lastWasShort) {
+                        println()
+                        lastWasShort = false
+                    }
+                    println("Year $year, Day $day")
+                    val method = lookup.findStatic(cls, mainName, mainMT)
+                    method.invokeExact()
+                    println()
+                } else {
+                    println("Year $year, Day $day: not yet")
+                    lastWasShort = true
+                }
             }
         }
     }
+    if (lastWasShort) {
+        println()
+    }
+    println("Total: $totalDays days in ${totalTime.inWholeSeconds} s")
 }
